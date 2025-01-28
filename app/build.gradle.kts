b578448f41316c2ea86cc8ba5231e8a7b2e47aa0
Krisztian0801 <bordacs.krisztian.2020i@bankitatabanya.hu>
2025-01-28 15:43:18 +0100

    Android version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "hu.krisztian.offthebeatenpath"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "hu.krisztian.offthebeatenpath"
         minSdk = 27
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
