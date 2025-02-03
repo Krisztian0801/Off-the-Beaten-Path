e19acea02ddec980740b045d1dda7f535008d187
Krisztian0801 <bordacs.krisztian.2020i@bankitatabanya.hu>
2025-02-03 19:31:14 +0100

    Navbar top and bottom

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,9 +12,13 @@ android {
         minSdk = 27
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.1"
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 
     buildTypes {
--- app/src/main/java/hu/krisztian/offthebeatenpath/MainActivity.kt
+++ app/src/main/java/hu/krisztian/offthebeatenpath/MainActivity.kt
@@ -1,20 +1,86 @@
 package hu.krisztian.offthebeatenpath
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.ActionMode
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.MaterialToolbar
+import hu.krisztian.offthebeatenpath.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var materialToolbar: MaterialToolbar
+
+
+    private var settingsString = ""
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var homeString = resources.getString(R.string.home)
+        var mapString = resources.getString(R.string.map)
+        var profileString = resources.getString(R.string.profile)
+        replaceFragment(HomeFragment(), homeString)
+        binding.footer.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment(), homeString)
+                R.id.map -> replaceFragment(MapFragment(), mapString)
+
+                R.id.profile -> replaceFragment(ProfileFragment(), profileString)
+
+                else -> {
+
+                }
+            }
+            true
         }
+        val callback = object : ActionMode.Callback {
+
+            override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+                menuInflater.inflate(R.menu.top_bar, menu)
+                return true
+            }
+
+            override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+                return false
+            }
+
+            override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+                settingsString = resources.getString(R.string.settings)
+                return when (item?.itemId) {
+                    R.id.settings -> {
+                        replaceFragment(SettingsFragment(), settingsString)
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+
+            override fun onDestroyActionMode(mode: ActionMode?) {
+            }
+        }
+        val actionMode = startSupportActionMode(callback)
+    }
+
+
+    private fun replaceFragment(fragment: Fragment, screen: String) {
+        materialToolbar = findViewById(R.id.topAppBar)
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        materialToolbar.setTitle(screen)
+        fragmentTransaction.commit()
+
     }
 }
