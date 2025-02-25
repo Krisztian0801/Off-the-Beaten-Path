61a0c8d8e3875807779d9c6baca7f7d0da0428a4
Krisztian0801 <bordacs.krisztian.2020i@bankitatabanya.hu>
2025-02-25 15:18:28 +0100

    Legal Docs

--- app/src/main/java/hu/krisztian/offthebeatenpath/LegalFragment.kt
+++ app/src/main/java/hu/krisztian/offthebeatenpath/LegalFragment.kt
@@ -1,10 +1,14 @@
 package hu.krisztian.offthebeatenpath
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class LegalFragment : Fragment() {
 
@@ -12,6 +16,28 @@ class LegalFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_legal, container, false)
+        val view = inflater.inflate(R.layout.fragment_legal, container, false)
+        val listView: ListView = view.findViewById(R.id.legalListView)
+
+        val items = arrayOf("Terms and Conditions", "Privacy Policy", "Copyright")
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, items)
+        listView.adapter = adapter
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            val url = when (position) {
+                0 -> "https://drive.google.com/file/d/1EFEgVRr1INZ93KPefOLMIGw3issQfU5Z/view" // Terms and Conditions
+                1 -> "https://drive.google.com/file/d/1XZWJ6GgoNPspwAy9SNGSQhZka4C5V7MI/view" // Privacy Policy
+                2 -> "https://drive.google.com/file/d/10t56U2YpdmWDQk-EORSrXpg-AXBKkYb8/view" // Copyright
+                else -> null
+            }
+
+            url?.let {
+                val intent = Intent(requireContext(), WebViewActivity::class.java)
+                intent.putExtra("pdf_url", it)
+                startActivity(intent)
+            }
+        }
+
+        return view
     }
 }
---
+++ app/src/main/java/hu/krisztian/offthebeatenpath/WebViewActivity.kt
@@ -0,0 +1,47 @@
+package hu.krisztian.offthebeatenpath
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class WebViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+
+        // Toolbar setup
+        val toolbar: Toolbar = findViewById(R.id.webViewToolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val webView: WebView = findViewById(R.id.webView)
+        val pdfUrl = intent.getStringExtra("pdf_url")
+
+        if (pdfUrl != null) {
+            webView.settings.javaScriptEnabled = true
+            webView.settings.cacheMode = WebSettings.LOAD_NO_CACHE
+            webView.settings.setSupportZoom(true)
+            webView.settings.builtInZoomControls = true
+            webView.settings.displayZoomControls = false
+            webView.webViewClient = WebViewClient()
+
+            val googleDocsViewer = "https://docs.google.com/gview?embedded=true&url=$pdfUrl"
+            //webView.loadUrl(googleDocsViewer)
+            webView.loadUrl(pdfUrl)
+        }
+    }
+
+    // Handle toolbar back button
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
