934db17d9cd011ea6dc32c4cbbee0575c7d94d8c
Krisztian0801 <bordacs.krisztian.2020i@bankitatabanya.hu>
2025-03-04 13:54:39 +0100

    feat: Implement profile render

--- app/src/main/java/hu/krisztian/offthebeatenpath/ProfileFragment.kt
+++ app/src/main/java/hu/krisztian/offthebeatenpath/ProfileFragment.kt
@@ -1,59 +1,55 @@
 package hu.krisztian.offthebeatenpath
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import hu.krisztian.offthebeatenpath.databinding.FragmentProfileBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        // 🔹 Itt hívjuk meg a `loadUserData()` függvényt!
+        loadUserData()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
+
+
+    private fun loadUserData() {
+        val sharedPreferences = requireContext().getSharedPreferences("MyAppPrefs", Context.MODE_PRIVATE)
+
+        val userName = sharedPreferences.getString("user_name", "Unknown User")
+        val userEmail = sharedPreferences.getString("user_email", "unknown@example.com")
+        val userImage = sharedPreferences.getString("user_image", null)
+
+        Log.d("ProfileFragment", "Loaded user data: name=$userName, email=$userEmail, image=$userImage")
+
+        binding.userNameTextView.text = userName
+        binding.userEmailTextView.text = userEmail
+
+        Glide.with(this)
+            .load(userImage)
+            .placeholder(R.drawable.user)
+            .error(R.drawable.user)
+            .into(binding.userImageView)
+    }
+
 }
