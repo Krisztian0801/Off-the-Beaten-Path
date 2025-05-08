b87a70f849beda00a6b1a6fe90f7c03d7fc19356
Krisztian0801 <bordacs.krisztian.2020i@bankitatabanya.hu>
2025-05-08 20:15:43 +0200

    version: version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "1.5.0"
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
